c33e7dd7c50665e2d1dcb15102d46bf98efdd844
damir <damirdamir692@gmail.com>
2024-05-06 02:02:58 +0500

    44 Using Async Pipe to Subscribe to Observables, redirecting from add page to categories page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import {NgModule} from "@angular/core";
+import {CategoryListComponent} from "./features/category/category-list/category-list.component";
+import {AddCategoryComponent} from "./features/category/add-category/add-category.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'admin/categories',
+    component: CategoryListComponent
+  },
+  {
+    path: 'admin/categories/add',
+    component: AddCategoryComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutes { }
