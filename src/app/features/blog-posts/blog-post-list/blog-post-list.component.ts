7e9b44b3cc2ec74b5d916dabd3d0bd6718b08554
damir <damirdamir692@gmail.com>
2024-09-12 15:07:52 +0500

    65 end list of Blogpost UI

--- src/app/features/blog-posts/blog-post-list/blog-post-list.component.ts
+++ src/app/features/blog-posts/blog-post-list/blog-post-list.component.ts
@@ -1,15 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {BlogPostService} from "../services/blog-post.service";
+import {Observable} from "rxjs";
+import {BlogPost} from "../models/blog-post.model";
+import {AsyncPipe, NgForOf, NgIf} from "@angular/common";
+
 
 @Component({
   selector: 'app-blog-post-list',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    NgIf,
+    AsyncPipe,
+    NgForOf
   ],
   templateUrl: './blog-post-list.component.html',
   styleUrl: './blog-post-list.component.css'
 })
-export class BlogPostListComponent {
+
+export class BlogPostListComponent implements OnInit {
+
+  blogPosts$?: Observable<BlogPost[]>;
+
+  constructor(private BlogPostService: BlogPostService) {
+
+  }
+
+  ngOnInit() {
+
+    //get all blog posts from API
+    this.blogPosts$ = this.BlogPostService.getAllBlogPosts()
+
+  }
 
 }
