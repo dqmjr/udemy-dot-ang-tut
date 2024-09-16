3a01e9a51fb6d3c325faa85228c8a4df62c04a21
damir <damirdamir692@gmail.com>
2024-09-16 18:28:04 +0500

    74 end | adding select form to pick categories

--- src/app/features/blog-posts/add-blog-post/add-blog-post.component.ts
+++ src/app/features/blog-posts/add-blog-post/add-blog-post.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AddCategoryRequest} from "../../category/models/add-category-request.model";
 import {AddBlogPost} from "../models/add-blog-post.model";
 import {FormsModule} from "@angular/forms";
-import {DatePipe, NgIf} from "@angular/common";
+import {AsyncPipe, DatePipe, NgForOf, NgIf} from "@angular/common";
 import {BlogPostService} from "../services/blog-post.service";
 import {Router} from "@angular/router";
 import {MarkdownModule} from "ngx-markdown";
+import {CategoryService} from "../../category/services/category.service";
+import {Observable} from "rxjs";
+import {Category} from "../../category/models/category.model";
 
 @Component({
   selector: 'app-add-blog-post',
@@ -14,16 +17,22 @@ import {MarkdownModule} from "ngx-markdown";
     FormsModule,
     DatePipe,
     MarkdownModule,
-    NgIf
+    NgIf,
+    AsyncPipe,
+    NgForOf
   ],
   templateUrl: './add-blog-post.component.html',
   styleUrl: './add-blog-post.component.css'
 })
-export class AddBlogPostComponent {
+export class AddBlogPostComponent implements OnInit{
 
   model: AddBlogPost;
+  categories$?: Observable<Category[]>;
 
-  constructor(private blogPostService: BlogPostService, private router: Router) {
+  constructor(private blogPostService: BlogPostService,
+              private router: Router,
+              private categoryService: CategoryService,
+  ) {
     this.model = {
       title: '',
       urlHandle: '',
@@ -35,6 +44,10 @@ export class AddBlogPostComponent {
       isVisible: true,
     }
   }
+
+  ngOnInit(): void {
+      this.categories$ = this.categoryService.getAllCategories();
+    }
 
   onFormSubmit() {
     this.blogPostService.createBlogPost(this.model)
